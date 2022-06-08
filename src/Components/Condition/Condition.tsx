9cb697874370c664aa022836c903eac3cf70ddf6
David Wong <davidwong369@outlook.com>
2022-06-08 16:14:41 +1200

    Update tab spacing
    Change tab spacing from 4 spaces to 2

--- src/Components/Condition/Condition.tsx
+++ src/Components/Condition/Condition.tsx
@@ -3,20 +3,24 @@ import 'fontsource-roboto';
 import { Typography } from '@material-ui/core';
 import './Condition.css';
 
-const Condition = (props: { responseObj: { found: boolean; text: React.ReactNode; }; }) => {
-   return (
-       <div>
-           {props.responseObj.found === true ?
-               <div className="content">
-                   <Typography>
-                       {props.responseObj.text}
-                    </Typography>
-               </div>
-           : <div className="content">
-               <Typography>Enter a number between 0-100 to learn a fun number fact!</Typography>
-            </div>
-           }
-       </div>
-   )
+const Condition = (props: { responseObj: { found: boolean; text: string}; }) => {
+  return (
+    <div>
+      {props.responseObj.found === true ?
+        <div className="content">
+          <Typography>
+            {props.responseObj.text}
+          </Typography>
+        </div>
+        :
+        <div className="content">
+          <Typography>
+            Enter a number between 0-100 to learn a fun number fact
+          !</Typography>
+        </div>
+      }
+   </div>
+  )
 }
 export default Condition;
+
--- src/Components/Number/Number.tsx
+++ src/Components/Number/Number.tsx
@@ -5,61 +5,56 @@ import  './Number.css';
 import { TextField, Button, Container } from '@material-ui/core'
 
 const Number = () => {
-    let [number, setNumber] = useState('')
-    let [responseObj, setResponseObj] = useState(Object);
+  let [number, setNumber] = useState('')
+  let [responseObj, setResponseObj] = useState(Object);
 
+  const uriEncodedNumber = encodeURIComponent(number)
 
-    const uriEncodedNumber = encodeURIComponent(number)
+  function getNumber(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    fetch(`http://numbersapi.com/${uriEncodedNumber}/trivia?json`)
+    .then(responseObj =>	responseObj.json())
+    .then(responseObj => {
+      setResponseObj(responseObj)
+    })
+  }
 
+  return (
+    <div className = "responseContainer">
 
-    function getNumber(e: { preventDefault: () => void; }) {
-        e.preventDefault();
-           fetch(
-                `http://numbersapi.com/${uriEncodedNumber}/trivia?json`)
-            .then(responseObj =>
-                responseObj.json())
-            .then(responseObj => {
-                setResponseObj(responseObj)
-            })
-        } 
+      <Condition responseObj = {responseObj}/>
 
-    return (
-        <div className = "responseContainer">
+      <Container>
+        <form onSubmit={getNumber} id = "formContainer">
+          <TextField
+            id = "numberInput"
+            label = "Number"
+            variant = "outlined"
+            type = "number"
+            InputProps={{
+              inputProps: {
+                max: 100, min: 0
+              }
+            }}
+            placeholder="Enter Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
 
-            <Condition responseObj = {responseObj}/>
-
-            <Container>
-            <form onSubmit={getNumber} id = "formContainer">
-                    <TextField
-                    id = "numberInput"
-                    label = "Number"
-                    variant = "outlined"
-                    type = "number"
-                    InputProps={{
-                        inputProps: { 
-                            max: 100, min: 0 
-                        }
-                    }}
-                    placeholder="Enter Number"
-                    value={number}
-                    onChange={(e) => setNumber(e.target.value)}/>
-
-
-                    <Button
-                    type = "submit"
-                    variant = "contained"
-                    size = "small"
-                    color = "primary"
-                    id = "submitButton">
-
-                        Get Number Fact
-                    </Button>
-
-            </form>
-
-            </Container>
-       </div>
-    )
+        <Button
+          type = "submit"
+          variant = "contained"
+          size = "small"
+          color = "primary"
+          id = "submitButton"
+        >
+          Get Number Fact
+        </Button>
+      </form>
+    </Container>
+  </div>
+  )
 }
 
 export default Number
+
